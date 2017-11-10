fa0be594996d5da2f42ecf601337b4b2f4dd1a35
bvalchev <30438891+bvalchev@users.noreply.github.com>
2017-11-10 23:27:21 +0200

    2 Ways of Queue implementation
    2 Ways of Queue implementation

---
+++ Queue.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+template<typename T>
+struct node {
+	node<T>* next;
+	T data;
+};
+template<typename T>
+class Queue {
+private:
+	node<T>* head;
+	node<T>* tail;
+	void copy(Queue<T> const& q) {
+		for (node<T>* p = q.head; p != NULL; p = p->next)
+			push(p->data);
+	}
+	void eraseQueue() {
+		node<T>* p = head;
+		while (p) {
+			p = p->next;
+			pop();
+		}
+	}
+public:
+	Queue() {
+		head = nullptr;
+		tail = nullptr;
+	}
+	Queue(Queue<T> const& other) {
+		copy(other);
+	}
+	Queue& operator = (Queue<T> const& other) {
+		if (this != &other) {
+			eraseQueue();
+			copy(other);
+		}
+		return *this;
+	}
+	~Queue() {
+		eraseQueue();
+	}
+	bool isEmpty() {
+		return (head==nullptr && tail==nullptr);
+	}
+	void push(T const& x) {
+		node<T>* p = new node<T>;
+		p->data = x;
+		p->next = nullptr;
+		if (isEmpty()) {
+			head = tail = p;
+		}
+		else {
+			tail->next = p;
+			tail = p;
+		}
+	}
+	int pop() {
+		if (isEmpty()) {
+			cerr << "The queue is empty" << endl;
+			return 0;
+		}
+		node<T>* p = head;
+		head = head->next;
+		T x = p->data;
+		delete p;
+		return x;
+	}
+	void print() {
+		node<T>* p = head;
+		while (p)
+		{
+			cout << p->data << " ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+};
+int main() {
+	Queue<int> a;
+	a.push(3);
+	a.push(4);
+	a.push(5);
+	a.print();
+	Queue<int> b = a;
+	b.print();
+	Queue<int> c(a);
+	c.print();
+	a.pop();
+	a.print();
+	system("pause");
+	return 0;
+}
---
+++ SpecialQueue.cpp
@@ -0,0 +1,80 @@
+#include "stdafx.h"
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+template<typename T>
+class SpecialQueue {
+private:
+	stack<T> toPush;
+	stack<T> toPop;
+public:
+
+	void enqueue(T x) {
+		//toPush.push(x);
+		while (!toPop.empty()) {
+			toPush.push(toPop.top());
+			toPop.pop();
+		}
+		toPop.push(x);
+		while (!toPush.empty()) {
+			toPop.push(toPush.top());
+			toPush.pop();
+		}
+	}
+	void dequeue() {
+		toPop.pop();
+		/*while (!toPush.empty()) {
+			toPop.push(toPush.top());
+			toPush.pop();
+		}
+		T t = toPop.top();
+		toPop.pop();
+		while (!toPop.empty()) {
+			toPush.push(toPop.top());
+			toPop.pop();
+		}*/
+	}
+	T printTop() {
+		return toPop.top();
+	}
+};
+
+
+int main()
+{
+	SpecialQueue<int> a;
+	vector<int> toPrint;
+	int counter = -1;
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		switch (x)
+		{
+		case 1: {
+			int p;
+			cin >> p;
+			a.enqueue(p);
+			break;
+		}
+		case 2: {
+			a.dequeue();
+			break;
+		}
+		case 3: {
+			toPrint.push_back(a.printTop());
+			break;
+		}
+		default:
+			break;
+		}
+	}
+	for (int l = 0; l < toPrint.size(); l++) {
+		cout << toPrint[l] << endl;
+	}
+	system("pause");
+	return 0;
+}
